b5fff6e99de9cd25bf53c9f35eb1c5e3771c6da2
mitacheto12 <just_1_die@abv.bg>
2022-09-13 11:29:52 +0300

    mylist

---
+++ ImplementMyList/ImplementMyList/MyListT.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+
+namespace ImplementMyList
+{
+    internal class MyList
+    {
+
+        private const int count = 2;
+
+        private int[] Listarry;
+        public MyList()
+        {
+            Listarry = new int[count];  
+
+        }
+        public int Index { get;set;}    
+        
+
+        public void Add(int elemet)
+        {
+            if (Index==Listarry.Length)
+            {
+                int []temArr=new int[Listarry.Length*2];
+               for (int i = 0; i < Listarry.Length; i++)
+               {
+                    temArr[i] = Listarry[i];    
+               }
+                Listarry = temArr;
+            }
+            
+            Listarry[Index++] = elemet;
+        }
+        public int RemoveAT(int index)
+        {
+
+            if (index>Index)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            int element = Listarry[index];
+            Listarry[index] = 0;
+            for (int i = index; i < Index; i++)
+            {
+                Listarry[i] = Listarry[i + 1];
+            }
+            Index--;
+            if (Index<=Listarry.Length/4)
+            {
+                int[] temarr = new int[Listarry.Length / 2];
+                for (int i = 0; i < Index; i++)
+                {
+                    temarr[i] = Listarry[i];
+                }
+                Listarry = temarr;
+            }
+
+            return element;
+        }
+        public bool Contains(int element)
+        {
+           
+            for (int i = 0; i < Listarry.Length; i++)
+            {
+                if (Listarry[i]==element)
+                {
+                    return true;  
+                }
+            }
+            return false;
+        }
+        
+        public void Print()
+        {
+
+            foreach (var item in Listarry)
+            {
+                if (item!=0)
+                {
+                    Console.Write(item+" ");
+
+                }
+                
+            }
+            Console.WriteLine();
+
+
+        }
+        public void Swap(int first,int second)
+        {
+            if (first<0 ||first>=Index ||second<0 ||second>=Index)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            int temp = Listarry[first];
+            Listarry[first] = Listarry[second];
+            Listarry[second] = temp;    
+           
+
+           
+        }
+
+    }
+}
+    
---
+++ ImplementMyList/ImplementMyList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementMyList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList();
+            list.Add(5);
+            list.Add(10);
+            list.Add(7);
+            list.Add(10);
+            list.Add(12);
+         
+            list.Print();
+           
+            
+
+
+
+        }
+    }
+}
